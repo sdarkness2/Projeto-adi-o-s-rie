f7f8529182b5e4140f3024891355b3386c89162a
José Carlos <jcnv.trabalho@gmail.com>
2021-02-24 12:37:19 -0300

    projeto de adição de séries

---
+++ Classes/Serie.cs
@@ -0,0 +1,76 @@
+using System;
+
+namespace Series
+{
+    public class Serie : EntidadeBase
+    {
+        //Atributos
+        public Serie(Genero genero, string titulo, int ano)
+        {
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Ano = ano;
+
+        }
+        public Serie(Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+
+        }
+        private Genero Genero { get; set; }
+
+        private string Titulo { get; set; }
+
+        private string Descricao { get; set; }
+
+        private int Ano { get; set; }
+
+        private bool Excluido { get; set; }
+
+        //Métodos
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Genero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano;
+            retorno += "Excluido: " + this.Excluido;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        internal int retornaId()
+        {
+            return this.Id;
+        }
+
+        internal bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+
+    }
+}
